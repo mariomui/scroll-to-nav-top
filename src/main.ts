a99d75f15a254da53f78089880368495dc5d2d67
mariomui <yi.kan.mui@gmail.com>
2025-05-24 07:08:38 -0700

    Add command icon to ribbon bar

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async onload() {
+		// https://lucide.dev/icons/circle-fading-arrow-up
+		this.addRibbonIcon(
+			"circle-fading-arrow-up",
+			"Scroll To Nav Top",
+			() => {
+				this.scrollToTop();
+			}
+		);
+
 		this.addCommand({
 			id: "scroll-to-nav-top",
 			name: "Scroll To Nav Top",
